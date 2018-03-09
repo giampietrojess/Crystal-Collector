7dd5179f01cd007b8cb2d95bfc31c122c7a25a7f
Jess Giampietro <JessGiampietro@Jesss-MacBook-Pro.local>
2018-03-09 03:09:03 -0500

    add reset function

--- assets/game.js
+++ assets/game.js
@@ -1,35 +1,61 @@
 var random_result;
-var wins;
-var losses;
+var wins = 0;
+var losses = 0;
 var images = ['assets/images/Crystal1.png', 'assets/images/Crystal2.png', 'assets/images/Crystal3.png', 'assets/images/Crystal4.png'];
+var previous = 0;
 
 // Create a game with four crystals that have a random value 
 
-random_result = Math.floor(Math.random() * 101 ) + 19;
-$("#result").html(random_result);
+var resetAndStart = function () {
+    $(".crystals").empty();
 
-// Create four crystals with 4 random numbers randomly on the page
-for(var i = 0; i < 4; i++){
-    var random = Math.floor(Math.random() * 12) +1;
-    
-    var crystals = $("<div>");
-        crystals.attr({
-        "class": ['crystals'],    
-        "data-random": random
-    });
-        crystals.css({
-            "background-image":"url('" + images[i] + "')",
-            "background-size":"cover"
+    random_result = Math.floor(Math.random() * 101 ) + 19;
+    $("#result").html(random_result);
+
+    // Create four crystals with 4 random numbers randomly on the page
+    for(var i = 0; i < 4; i++){
+        var random = Math.floor(Math.random() * 12) +1;
+        
+        var crystals = $("<div>");
+            crystals.attr({
+            "class": ['crystals'],    
+            "data-random": random
         });
+            crystals.css({
+                "background-image":"url('" + images[i] + "')",
+                "background-size":"cover"
+            });
 
-    $(".crystalbox").append(crystals)
+        $(".crystalbox").append(crystals)
 
-}
-
-$(".crystals").on('click', function() {
-    console.log($(this));
-
+    }
+}    
+resetAndStart();
 
 
+
+// When you click the crystal
+$(document).on('click', ".crystals", function() {
+    // Apply the data value to a number
+    var num =  parseInt($(this).attr('data-random'));
+    // Take the previous number and add it to the current number
+    previous += num;
+    // If the previous number is greater than the random result, add loss
+    if(previous > random_result){
+        losses++;
+        $("#loser").html(losses);
+        previous = 0;
+        resetAndStart();
+    }
+    // If the previous number is equal to the random result, add win
+    else if(previous === random_result) {
+        wins++;
+        $("#winner").html(wins);
+        previous = 0;
+        resetAndStart();
+    }
 
 });
+
+
+
